e5a3a676b9411066b409d45e41f0a08d7f892a00
hassan-momen <hmomen8@gmail.com>
2018-05-07 13:20:56 +0200

    added Advanced

---
+++ Advanced/myscript.js
@@ -0,0 +1,5 @@
+ $(document).ready(function(){
+       $("a").attr("href","http://www.codefactory.wien/");
+       $("p>code").css("color","red");
+       $("ol>li").css("background-color","#2a7b90");
+     });
